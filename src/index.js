dc884afbd2aabfecee0c4db2adf37e1675fbc073
Lazytangent <pm123633@gmail.com>
2021-04-20 22:54:35 -0500

    Add store and actions to window

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import './normalize.css';
 import './index.css';
 import App from './App';
 import configureStore from './store';
 
+// import * as cardActions from './store/card';
+import { getRandomCard } from './store/card';
+
 const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+    window.getRandomCard = getRandomCard;
+}
 
 ReactDOM.render(
     <StrictMode>
